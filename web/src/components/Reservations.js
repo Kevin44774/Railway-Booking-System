78cbdd7a59996109dbd8dae8cd4d09d7f433fe29
Kevin Jose <126767172+Kevin44774@users.noreply.github.com>
2024-07-22 17:44:08 +0530

    Reservations.js

--- web/src/components/Reservations.js
+++ web/src/components/Reservations.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-
-import { Row, Col, Button, Card, Pagination } from 'react-bootstrap'
-import { getReservations, deleteReservation } from '../Services'
-import { toast } from 'react-toastify'
+import { Row, Col, Button, Card, Pagination, Form } from 'react-bootstrap';
+import { getReservations, deleteReservation } from '../Services';
+import { toast } from 'react-toastify';
 
 class Reservations extends Component {
-
     constructor(props) {
         super(props);
         this.state = {
@@ -13,57 +11,62 @@ class Reservations extends Component {
             items: [],
             offset: 1,
             lastPage: 1,
-            paginateItems: []
+            paginateItems: [],
+            searchQuery: '',
         };
     }
 
     componentDidMount() {
-        this.updateReservations()
+        this.updateReservations();
     }
 
-    componentWillUpdate() {
-        var user = localStorage.getItem('user')
-        if (!user) {
-            this.props.history.push('/')
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.searchQuery !== this.state.searchQuery || prevState.offset !== this.state.offset) {
+            this.paginateReservations();
         }
     }
 
     updateReservations = () => {
-        var user = localStorage.getItem('user')
+        const user = localStorage.getItem('user');
         if (!user) {
-            this.props.history.push('/')
+            this.props.history.push('/');
         } else {
-            user = JSON.parse(user)
-            getReservations(user._id)
+            const userId = JSON.parse(user)._id;
+            getReservations(userId)
                 .then(res => {
-                    this.setState({ reservations: res }, () => this.paginateReservations())
+                    this.setState({ reservations: res }, () => this.paginateReservations());
                 })
                 .catch(err => {
-                    console.log(err)
-                })
+                    console.log(err);
+                });
         }
     }
 
     cancelReservation = id => {
-        var c = window.confirm("The reservation " + id + " will be deleted")
+        const c = window.confirm("The reservation " + id + " will be deleted");
         if (c) {
             deleteReservation(id)
                 .then(res => {
-                    toast.success("Successfully removed reservation " + id)
-                    this.updateReservations()
+                    toast.success("Successfully removed reservation " + id);
+                    this.updateReservations();
                 })
                 .catch(err => {
-                    console.log(err)
-                })
+                    console.log(err);
+                });
         }
     }
 
     paginateReservations = () => {
+        const filteredReservations = this.state.reservations.filter(reservation => 
+            reservation.from.toLowerCase().includes(this.state.searchQuery.toLowerCase()) ||
+            reservation.to.toLowerCase().includes(this.state.searchQuery.toLowerCase())
+        );
+
         let items = [];
-        const offset = (this.state.offset - 1) * 5
+        const offset = (this.state.offset - 1) * 5;
 
         for (let number = offset; number < offset + 5; number++) {
-            const reservation = this.state.reservations[number]
+            const reservation = filteredReservations[number];
             if (reservation) {
                 items.push(
                     <Row style={{ width: '75%' }} key={number}>
@@ -100,29 +103,44 @@ class Reservations extends Component {
                             </Card>
                         </Col>
                     </Row>
-                )
+                );
             }
         }
+
+        const lastPage = Math.ceil(filteredReservations.length / 5);
         let paginateItems = [];
-        const lastPage = Math.ceil(this.state.reservations.length / 5)
         for (let number = 1; number <= lastPage; number++) {
             paginateItems.push(
                 <Pagination.Item key={number} active={number === this.state.offset} onClick={() => this.pageChange(number)}>
                     {number}
-                </Pagination.Item>,
+                </Pagination.Item>
             );
         }
-        this.setState({ paginateItems: paginateItems, items: items, lastPage: lastPage })
+
+        this.setState({ paginateItems: paginateItems, items: items, lastPage: lastPage });
     }
 
     pageChange = n => {
-        console.log(n)
-        this.setState({ offset: n }, () => this.paginateReservations())
+        this.setState({ offset: n });
+    }
+
+    handleSearch = event => {
+        this.setState({ searchQuery: event.target.value, offset: 1 }, () => this.paginateReservations());
     }
 
     render() {
         return (
             <Row style={{ alignItems: 'center', justifyContent: 'center', width: '100%' }}>
+                <Row style={{ width: '75%', padding: 10 }}>
+                    <Col>
+                        <Form.Control
+                            type="text"
+                            placeholder="Search by 'From' or 'To'"
+                            value={this.state.searchQuery}
+                            onChange={this.handleSearch}
+                        />
+                    </Col>
+                </Row>
                 {this.state.reservations.length <= 0 &&
                     <Row style={{ width: '75%', padding: 10 }}>
                         <Col>
@@ -141,11 +159,9 @@ class Reservations extends Component {
                                 <Pagination.Last onClick={() => this.pageChange(this.state.lastPage)} />
                             </Pagination>
                         </Row>
-                        {this.state.items.map((reservation, i) => {
-                            return (
-                                reservation
-                            )
-                        })}
+                        {this.state.items.map((reservation, i) => (
+                            reservation
+                        ))}
                         <Row style={{ width: '75%', paddingTop: 20, paddingLeft: 15 }}>
                             <Pagination>
                                 <Pagination.First onClick={() => this.pageChange(1)} />
